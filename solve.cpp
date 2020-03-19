d14e9dd10143357f7a96a7fc08d1a3b636fcdd96
huang-me <amos0107@gmail.com>
2020-03-19 22:34:30 +0800

    return

--- solve.cpp
+++ solve.cpp
@@ -19,33 +19,33 @@ int main() {
     }
 
     // solve the sudoku
-    // int sol = sudoku.solve();
-    // if(sol == 0) {
+    int sol = sudoku.solve();
+    if(sol == 0) {
 
-    //     // test whether there's still solution
-    //     // for(int i=0;i<9;i++) {
-    //     //     for(int j=0;j<9;j++) {
-    //     //         for(int k=1;k<10;k++) {
-    //     //             if(sudoku.exist[i][j][k] == 0) {
-    //     //                 cout << "2" << endl;
-    //     //                 return 0;
-    //     //             }
-    //     //         }
-    //     //     }
-    //     // }
+        // test whether there's still solution
+        // for(int i=0;i<9;i++) {
+        //     for(int j=0;j<9;j++) {
+        //         for(int k=1;k<10;k++) {
+        //             if(sudoku.exist[i][j][k] == 0) {
+        //                 cout << "2" << endl;
+        //                 return 0;
+        //             }
+        //         }
+        //     }
+        // }
 
-    //     // show matrix
-    //     cout << "1" << endl;
-    //     for(int i=0; i<9; i++) {
-    //         for(int col=0; col<9; col++) {
-    //             cout << to_string(sudoku.intMatrix[i][col]);
-    //             if(col < 8) cout << " ";
-    //         }
-    //         cout << endl;
-    //     }
+        // show matrix
+        cout << "1" << endl;
+        for(int i=0; i<9; i++) {
+            for(int col=0; col<9; col++) {
+                cout << to_string(sudoku.intMatrix[i][col]);
+                if(col < 8) cout << " ";
+            }
+            cout << endl;
+        }
 
-    // } 
-    // else if(sol == -1) cout << "0" << endl;
-    cout << "0\n";
+    } 
+    else if(sol == -1) cout << "0" << endl;
+
     return 0;
 }
