058065f913db759648dd802f3c5860bc884f127b
huang-me <amos0107@gmail.com>
2020-03-24 17:14:34 +0800

    test ans

--- sudoku.cpp
+++ sudoku.cpp
@@ -325,7 +325,7 @@ void Sudoku::solve() {
         trace(0);
         switch(_solvenum) {
         case 0://no solution
-            cout << 0;
+            cout << 2;
             // printboard();
             break;
             // case 1://only 1 solution
